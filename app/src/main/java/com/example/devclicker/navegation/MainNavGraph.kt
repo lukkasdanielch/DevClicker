a124501e6b3be71628545b8a4d9b1979b402a053
lukkasdanielch <l.daniel@cs.up.edu.br>
2025-11-05 20:43:20 -0300

    commit do maliki de navigate

--- app/src/main/java/com/example/devclicker/navegation/MainNavGraph.kt
+++ app/src/main/java/com/example/devclicker/navegation/MainNavGraph.kt
@@ -6,7 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.devclicker.ui.auth.login.LoginScreen
 import com.example.devclicker.ui.auth.signup.SignUpScreen
-import com.example.devclicker.ui.game.MainScreen // Importar a tela do jogo
+import com.example.devclicker.ui.game.MainScreen
 
 @Composable
 fun MainNavGraph(navController: NavHostController, startDestination: String) {
---
+++ app/src/main/java/com/example/devclicker/ui/game/game/GameViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.devclicker.ui.game.game
+
