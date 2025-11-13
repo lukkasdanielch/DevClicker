b2f74e149e148ceb92454fee0f457d9fd387c012
Malik Ribeiro <malik.mourad@grupoelectra.com.br>
2025-11-13 13:07:08 -0300

    Coloquei o "T" no SenttingsUistate.kt

--- app/src/main/java/com/example/devclicker/ui/navegation/GameNavGraph.kt
+++ app/src/main/java/com/example/devclicker/ui/navegation/GameNavGraph.kt
@@ -1,13 +1,10 @@
 package com.example.devclicker.navigation
 
 import androidx.compose.runtime.Composable
-// 1. (A CORREÇÃO) Importe o 'hiltViewModel'
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-// 2. (A CORREÇÃO) Remova o import antigo
-// import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.devclicker.ui.game.clicker.ClickerScreen
 import com.example.devclicker.ui.game.clicker.ClickerViewModel
 import com.example.devclicker.ui.game.settings.SettingsScreen
